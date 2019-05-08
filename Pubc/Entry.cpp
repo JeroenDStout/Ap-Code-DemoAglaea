JeroenDStout/Ap-Code-DemoClock

Add initial set-up

Pubc/Entry.cpp
@@ -1,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+#include "BlackRoot/Pubc/Math Types.h"
+#include "BlackRoot/Pubc/Threaded IO Stream.h"
+
+#include "ToolboxBase/Pubc/Entry.h"
+#include "ToolboxBase/Pubc/Environment Bootstrap.h"
+
+#include "DemoClock/Pubc/Version.h"
+#include "DemoClock/Pubc/Environment.h"
+
+#include "DemoClock/Pubc/Register.h"
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <complex>
+
+int Demo_Clock_Main(Toolbox::Util::EnvironmentBootstrap &bootstrap)
+{
+    using cout = BlackRoot::Util::Cout;
+    
+        // Introduce ourselves
+    cout{} << BlackRoot::Repo::VersionRegistry::GetBootString() << std::endl << std::endl;
+
+        // Create an environment and start its thread
+	DemoClock::Core::Environment * environment = new DemoClock::Core::Environment();
+	Toolbox::Core::Set_Environment(environment);
+	std::thread t1([=]{
+        environment->run_with_current_thread();
+    });
+    
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    bootstrap.setup_environment(environment);
+
+    if (!bootstrap.execute_from_boot_file()) {
+        cout{} << "Cannot recover from startup errors" << std::endl;
+    }
+
+    t1.join();
+
+    delete environment;
+
+    return 0;
+}
+
+TB_STARTFUNC_DEF(Demo_Clock_Main);

Pubc/Environment.cpp
@@ -1,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "BlackRoot/Pubc/Assert.h"
+
+#include "DemoClock/Pubc/Environment.h"
+
+using namespace DemoClock::Core;
+
+    //  Relay message receiver
+    // --------------------
+
+CON_RMR_DEFINE_CLASS(Environment);
+
+    //  Setup
+    // --------------------
+
+Environment::Environment()
+{
+}
+
+Environment::~Environment()
+{
+}

Pubc/Environment.h
@@ -1,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "ToolboxBase/Pubc/Base Environment.h"
+
+#include "IrisBase/Pubc/Interface Layouts.h"
+
+namespace DemoClock {
+namespace Core {
+    
+	class Environment : public Toolbox::Base::BaseEnvironment {
+        CON_RMR_DECLARE_CLASS(Environment, Toolbox::Base::BaseEnvironment);
+
+    protected:        
+            // Web
+		
+        std::string internal_get_favicon_name() override { return "demo_clock_favicon.ico"; }
+
+    public:
+        Environment();
+        ~Environment() override;
+	};
+
+}
+}

Pubc/Version.cpp
@@ -1,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "DemoClock/Pubc/Version.h"
+
+#include "DemoClock/-genc/def_repo_version.h"
+#include "DemoClock/-genc/def_contribute.h"
+
+namespace DemoClock {
+	namespace Core {
+		
+		BR_VERSION_DEFINE(DemoClock);
+        BR_CONTRIBUTE_DEFINE(DemoClock);
+        BR_PROJECT_DEFINE(DemoClock);
+
+	}
+} 

Pubc/Version.h
@@ -1,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "BlackRoot/Pubc/Version Reg.h"
+
+namespace DemoClock {
+namespace Core {
+
+	BR_VERSION_DECLARE(DemoClock);
+
+}
+}

JeroenDStout/Ap-Code-DemoAglaea

Pubc/Entry.cpp
@@ -1,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+#include "BlackRoot/Pubc/Math Types.h"
+#include "BlackRoot/Pubc/Threaded IO Stream.h"
+
+#include "ToolboxBase/Pubc/Entry.h"
+#include "ToolboxBase/Pubc/Environment Bootstrap.h"
+
+#include "DemoAglaea/Pubc/Version.h"
+#include "DemoAglaea/Pubc/Environment.h"
+
+#include "DemoAglaea/Pubc/Register.h"
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <complex>
+
+int Demo_Aglaea_Main(Toolbox::Util::EnvironmentBootstrap &bootstrap)
+{
+    using cout = BlackRoot::Util::Cout;
+    
+        // Introduce ourselves
+    cout{} << BlackRoot::Repo::VersionRegistry::GetBootString() << std::endl << std::endl;
+
+        // Create an environment and start its thread
+	DemoAglaea::Core::Environment * environment = new DemoAglaea::Core::Environment();
+	Toolbox::Core::Set_Environment(environment);
+	std::thread t1([=]{
+        environment->run_with_current_thread();
+    });
+    
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    bootstrap.setup_environment(environment);
+
+    if (!bootstrap.execute_from_boot_file()) {
+        cout{} << "Cannot recover from startup errors" << std::endl;
+    }
+
+    t1.join();
+
+    delete environment;
+
+    return 0;
+}
+
+TB_STARTFUNC_DEF(Demo_Aglaea_Main);

Pubc/Environment.cpp
@@ -1,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "BlackRoot/Pubc/Assert.h"
+
+#include "DemoAglaea/Pubc/Environment.h"
+
+using namespace DemoAglaea::Core;
+
+    //  Relay message receiver
+    // --------------------
+
+CON_RMR_DEFINE_CLASS(Environment);
+
+    //  Setup
+    // --------------------
+
+Environment::Environment()
+{
+}
+
+Environment::~Environment()
+{
+}

Pubc/Environment.h
@@ -1,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "ToolboxBase/Pubc/Base Environment.h"
+
+namespace DemoAglaea {
+namespace Core {
+    
+	class Environment : public Toolbox::Base::BaseEnvironment {
+        CON_RMR_DECLARE_CLASS(Environment, Toolbox::Base::BaseEnvironment);
+
+    protected:        
+            // Web
+		
+        std::string internal_get_favicon_name() override { return "demo_aglaea_favicon.ico"; }
+
+    public:
+        Environment();
+        ~Environment() override;
+	};
+
+}
+}

Pubc/Version.cpp
@@ -1,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "DemoAglaea/Pubc/Version.h"
+
+#include "DemoAglaea/-genc/def_repo_version.h"
+#include "DemoAglaea/-genc/def_contribute.h"
+
+namespace DemoAglaea {
+	namespace Core {
+		
+		BR_VERSION_DEFINE(DemoAglaea);
+        BR_CONTRIBUTE_DEFINE(DemoAglaea);
+        BR_PROJECT_DEFINE(DemoAglaea);
+
+	}
+} 

Pubc/Version.h
@@ -1,0 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "BlackRoot/Pubc/Version Reg.h"
+
+namespace DemoAglaea {
+namespace Core {
+
+	BR_VERSION_DECLARE(DemoAglaea);
+
+}
+}